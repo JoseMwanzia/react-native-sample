fd8db7e5fd3b26c4e07dda0293c494b70b39b8f0
joseph mwanzia <makamwanzia@gmail.com>
2024-08-03 17:25:55 +0300

    layout to use navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,32 @@
 import { Tabs } from 'expo-router';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Feather from '@expo/vector-icons/Feather';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import More from './more';
+import Settings from './settings';
+import RootLayout from './index';
 
 export default function TabLayout() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <Tabs screenOptions={{headerShown: false}}>
-      <Tabs.Screen name="(home)" options={{ 
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen component={RootLayout} name="index" options={{ 
         title: 'Home',
         tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
       }} />
 
 
-      <Tabs.Screen name="more" options={{
+      <Tab.Screen component={More} name="more" options={{
         title: 'More',
         tabBarIcon: ({ color }) => <Feather name="more-horizontal" size={28} color={color} />,
       }}/>
 
-      <Tabs.Screen name="settings" options={{
+      <Tab.Screen component={Settings} name="settings" options={{
         title: 'Settings',
         tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
       }}/>
 
-    </Tabs>
+    </Tab.Navigator>
   );
 }
