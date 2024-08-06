85f758af416cb51ed7068c7c29687500f9a23dd1
joseph mwanzia <makamwanzia@gmail.com>
2024-08-06 23:05:16 +0300

    run server

--- server/Index.js
+++ server/Index.js
@@ -48,3 +48,10 @@ app.post('/register', [
 
 });
 
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(`Server is running on port ${port}`);
+    }
+})
