f6b1f0b7e059ec6742d51a9f1573499fb4f47a3c
joseph mwanzia <makamwanzia@gmail.com>
2024-08-09 17:02:44 +0300

    get /users route

--- server/Index.js
+++ server/Index.js
@@ -16,6 +16,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error!' });
+    }
+});
+
+
 app.post('/register', [
     [
         body("username")
